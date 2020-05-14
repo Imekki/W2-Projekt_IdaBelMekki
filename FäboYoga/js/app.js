9a767dcca52618bfc8c3eb80370a4ec52e683730
Ida <ida@belmekki.com>
2020-05-14 16:30:48 +0200

    progress
    klar med nav, header, content. håller på med cards för section. Är inte säker på färgval till nav/footer. Försök fixa problem med backgrundsfärg

--- FäboYoga/js/app.js
+++ FäboYoga/js/app.js
@@ -14,3 +14,25 @@ const navSlide = () => {
 }
 
 navSlide();
+
+/*//Variables
+var i = 0;
+var images = [];
+var time = 4000;
+
+//img
+images[0] = 'img/buddha.jpg';
+images[1] = 'img/Helgbo.jpg';
+
+function pictureSlide() {
+    document.slide.src = images[i];
+
+    if(i < images.length - 1) {
+        i++;
+    } else {
+        i=0;
+    }
+    setTimeout("pictureSlide()", time);
+}
+
+window.onload = pictureSlide();*/
